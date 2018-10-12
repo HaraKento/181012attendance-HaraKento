d10bc336f8cb3bcb1c3f0db06a424c9d7534a75c
HaraKento <43873802+HaraKento@users.noreply.github.com>
2018-10-12 17:45:05 +0900

    after edit

--- fibonacci.py
+++ fibonacci.py
@@ -14,4 +14,16 @@ fib(10)
 
 print("n=2018のとき" + "f(n)=" + str(fib(2018)) + "である。")
 
+m=int(input())
+l=0
+while fib(l)<=m:
+    l+=1
+print(str(m)+"以下で最大のフィボナッチ数は、" + str(fib(l-1))+ "です。")
+    
 
+    
+
+
+
+ 
+    
